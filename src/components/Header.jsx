f511daaefb0cce08148d5e007365bb605d5f6630
Cássio Paixão <cassiopaixaoti@gmail.com>
2019-02-09 10:33:04 -0300

    add eslint and prettier

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => (
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
                 <a className="navbar-item" href="/">
-                    <img width="120" src={ParcelLogo} alt=""/>
+                    <img width="120" src={ParcelLogo} alt="parcel logo"/>
                 </a>
             </div>
         </nav>
