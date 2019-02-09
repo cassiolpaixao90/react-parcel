eb4119a8ccde365f3691dd48cee790c74586f8bd
Cássio Paixão <cassiopaixaoti@gmail.com>
2019-02-09 12:02:51 -0300

    eslint config

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-import React from "react";
-import { render } from "react-dom";
-import Header from "./components/Header";
-import "./scss/app.scss";
+import React from 'react';
+import { render } from 'react-dom';
+import PropTypes from 'prop-types';
+import Header from './components/Header';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <div className="container">
-          <h1>{this.props.name}</h1>
-          <h5>MODE in {process.env.NODE_ENV}</h5>
-        </div>
-      </div>
-    );
-  }
-}
+import './scss/app.scss';
+
+const App = props => (
+  <React.Fragment>
+    <Header />
+    <div className="container">
+      <h1>{props.name}</h1>
+      <h5>MODE in {process.env.NODE_ENV}</h5>
+    </div>
+  </React.Fragment>
+);
+
+App.propTypes = {
+  name: PropTypes.string.isRequired,
+};
 
 function renderApp() {
-  render(<App name="React + Parcel1" />, document.getElementById("root"));
+  render(<App name="React + Parcel1" />, document.getElementById('root'));
 }
 
 renderApp();
